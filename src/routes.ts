c94cf6f70fb75571b76c0b8b84232c6a826553df
Caio Almeida <caioalmeidanaweb@gmail.com>
2020-06-03 16:42:49 -0400

    complete code of day #1

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import ItemsController from "./controllers/ItemsController";
+import PointsController from "./controllers/PointsController";
+
+const routes = Router();
+
+routes.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+routes.get("items", ItemsController.index);
+
+routes.post("points", PointsController.create);
+routes.get("points", PointsController.index);
+routes.get("points/:id", PointsController.show);
+
+export default routes;
