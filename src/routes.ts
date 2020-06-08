7a0b62446acd526199d29ea5f920e140a347aa30
Caio Almeida <caioalmeidanaweb@gmail.com>
2020-06-07 21:34:22 -0400

    finalize with multer

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 import ItemsController from "./controllers/ItemsController";
 import PointsController from "./controllers/PointsController";
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.get("/", (request, response) => {
   return response.json({ message: "Hello World" });
@@ -10,7 +13,8 @@ routes.get("/", (request, response) => {
 
 routes.get("/items", ItemsController.index);
 
-routes.post("/points", PointsController.create);
+routes.post("/points", upload.single("image"), PointsController.create);
+
 routes.get("/points", PointsController.index);
 routes.get("/points/:id", PointsController.show);
 
